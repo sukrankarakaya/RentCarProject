7aefc5a08b4f967508b76e227e559736a1ca7bee
Şükran Karakaya <sukran.karakaya@ogrenci.selcuk.edu.tr>
2021-02-23 17:16:24 +0300

    10.Ders Ödev1

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public class Messages
+    {
+        public static string CarAdded = "Araba Eklendi";
+        public static string CarNameInvalid = "Araba ismi geçersiz.";
+        public static string CarDelede = "Araba silindi.";
+
+        public static string ColorAdded = "Color eklendi.";
+        public static string ColorDelete = "Color silindi";
+        public static string ColorUpdate = "Color Güncellendi.";
+
+        public static string BrandAdded = "Brand eklendi.";
+        public static string BrandDelete = "Brand silindi";
+        public static string BrandUpdate = "Brand Güncellendi.";
+        public static string MaintenanceTime="Sistemimiz bakımdadır.";
+        public static string CarListed= "Arabalar Listelendi.";
+    }
+}
---
+++ core/Utilities/Results/Result.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        private string carDelede;
+
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+            
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public Result()
+        {
+        }
+
+        public Result(string carDelede)
+        {
+            this.carDelede = carDelede;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
